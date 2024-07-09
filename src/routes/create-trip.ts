d75b3d33bf239b4f8ba557a8d23530258f269cc4
vtAndreola <dev.andreola@gmail.com>
2024-07-09 10:11:50 -0300

    feat: create post trip route w zod validation

---
+++ src/routes/create-trip.ts
@@ -0,0 +1,41 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import z from "zod";
+import { prisma } from "../lib/prisma";
+import dayjs from "dayjs";
+
+export async function createTrip(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    "/trips",
+    {
+      schema: {
+        body: z.object({
+          destination: z.string().min(4),
+          starts_at: z.coerce.date(),
+          ends_at: z.coerce.date(),
+        }),
+      },
+    },
+    async (req) => {
+      const { destination, starts_at, ends_at } = req.body;
+
+      if (dayjs(starts_at).isBefore(new Date())) {
+        throw new Error("Invalid trip start date");
+      }
+
+      if (dayjs(ends_at).isBefore(starts_at)) {
+        throw new Error("Invalid trip end date");
+      }
+
+      const trip = await prisma.trip.create({
+        data: {
+          destination,
+          starts_at,
+          ends_at,
+        },
+      });
+
+      return { tripID: trip.id };
+    }
+  );
+}
