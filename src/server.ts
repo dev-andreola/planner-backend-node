5ba43f474647bc933ca35020852559942350a152
vtAndreola <dev.andreola@gmail.com>
2024-07-09 09:01:16 -0300

    chore: prisma init

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,10 @@ import fastify from "fastify";
 
 const app = fastify();
 
+app.get("/", () => {
+  return "Hello World";
+});
+
 app.listen({ port: 3333 }).then(() => {
   console.log("Server running...");
 });
